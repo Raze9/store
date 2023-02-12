3899348faab577709a58fed9d511ed85ee38e5fe
genshin impact <87218053+Raze9@users.noreply.github.com>
2023-02-12 21:37:45 +0800

    Update mail.sql

--- mail/dao/mail.sql
+++ mail/dao/mail.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
--- Host: localhost    Database: db_temp
+-- Host: localhost    Database:   
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
